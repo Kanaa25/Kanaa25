d657631b91a62c5dab51a514e2bb61c104c229bb
Kanaa25 <karodyk@gmail.com>
2022-10-01 14:25:53 +0200

    cwiczenia2 added

---
+++ Python exercises/cwiczenia2.py
@@ -0,0 +1,45 @@
+#Create a dictionary that describes a person's contact information. Print name and phone number as one string with explanation. For example:
+
+print("Exercise 8")
+
+
+
+#“Bob Dylan, phone number: 000-000-000”
+#Create a list of  dictionaries that describe a worker (his name/surname, position, project etc). Print one of the worker’s information.
+#There is a string that describes employee information, e.g.:
+
+#“Lilly;Brown;QA Engineer;Samsung”
+#List values are separated with “;”.
+
+#Print out a string that represents name, surname, position and project from the given string with explanation. For example:
+
+#Name: Lilly, Surname: Brown. Position: QA Engineer, project: Samsung.
+#Note: you can use list indexes to form the output string.
+#You have a dictionary of dicitionaries:
+#{
+#     “Ford Fiesta”: {
+#          “year”: 2014,
+#          “color”: “Candy blue”
+#     # },
+#     “Seat Leon”: {
+#          “year”: 2018,
+#          “color”: “Desire red”
+#     }
+# }
+#
+# Form and print out the following string:
+# “You picked ‘Seat Leon’. Selected color: ‘Desire red’, year of production: 2018”
+# Create a dictionary that describes your laptop technical details (e.g. RAM, hard drive capacity etc.) Using the get() method of dictionaries print out existing and missing information.
+# You should use get() at least once for each case:
+# get(existing key)
+# get(missing key)
+# get(missing key, default value)
+# You have a string: “My name is Bob”. Using [:], split() and append() methods, create a string that replaces Bob with your name.
+#
+#
+# Think about 7 favourite dishes that you like. Now, you have to create a schedule of eating them each day. Create a dictionary with 7 keys (names of week days). For each key the value will be a dictionary containing the name of your favourite dish and list of ingredients that you have to buy to cook it.
+
+#Try to print out ingredients for different days of the week.
+#You have a list of foods, but you don’t know the order of the items (meaning that it can be: [“Strawberries”, “Potatoes”, “Karkóweczka”] or [“Potatoes”, “Karkóweczka”, “Strawberries”, “Sausage”]. Find your favourite food within the given list, print the food in a string. For example:
+#“I love Karkóweczka”
+
